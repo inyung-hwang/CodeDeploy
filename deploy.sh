6daf160f3832e2d6242a301b00394d900d658eef
Inyung Hwang <inyung.hwang@samsung.com>
2018-05-01 06:15:55 +0000

    no progress of S3 copying shown

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,18 @@ REGION=
 S3_BUCKET=
 
 # Download appspec yaml for AWS CodeDeploy
-aws s3 cp --region $REGION s3://$S3_BUCKET/appspec.yml .
+aws s3 cp --region $REGION s3://$S3_BUCKET/appspec.yml . --no-progress
 
 # Download executable JAR
-aws s3 cp --region $REGION s3://$S3_BUCKET/$EXEC_JAR .
+aws s3 cp --region $REGION s3://$S3_BUCKET/$EXEC_JAR . --no-progress
 
 # Download scripts to run at deploy time
-aws s3 sync --region $REGION s3://$S3_BUCKET/scripts scripts
+aws s3 sync --region $REGION s3://$S3_BUCKET/scripts scripts --no-progress
 
 # Bundles and uploads to S3 an application revision, which is a zip archive file
 # that contains deployable content and an accompanying Application Specification file.
 # A message is returned that describes how to call the create-deployment command
-# to deploy the application revvision from S3 to target EC2 instances.
+# to deploy the application revision from S3 to target EC2 instances.
 # BUNDLE_INFO saves --s3-location parameter from the message.
 BUNDLE_INFO=$(aws deploy push --application-name $APP_NAME \
         --region $REGION --s3-location s3://$S3_BUCKET/$BUNDLE \
@@ -38,3 +38,5 @@ aws deploy create-deployment \
         --region $REGION --application-name $APP_NAME \
         --deployment-group-name $DEPLOY_GROUP \
         --s3-location $BUNDLE_INFO
+
+echo "# Use the deploymentId when you need to perform a rollback operation."
